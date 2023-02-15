2dabbadff3104d00bb1f9be02a3e1aba230580c8
Rajab Karume <rkarume@gmail.com>
2023-02-15 12:50:11 +0300

    ✨created user model

---
+++ db/migrate/20230215094950_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :phone_number
+      t.string :email
+      t.string :previous_bill
+      t.string :current_bill
+      t.string :account_number
+
+      t.timestamps
+    end
+  end
+end
